8dec3c67e98762c6cc8dd87cdde9a87f501b00db
Mat-Algo <matthewmodisseril@gmail.com>
2023-09-05 20:17:38 +0530

    changes intext

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def handle_userinput(user_question):
 
 def main():
     load_dotenv()
-    st.set_page_config(page_title="Chat with multiple PDFs",
+    st.set_page_config(page_title="PDFAI by craftersAI",
                        page_icon=":books:")
     st.write(css, unsafe_allow_html=True)
 
@@ -75,8 +75,8 @@ def main():
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = None
 
-    st.header("Chat with multiple PDFs :books:")
-    user_question = st.text_input("Ask a question about your documents:")
+    st.header("Chat with your pdfs :books:")
+    user_question = st.text_input("Chat:")
     if user_question:
         handle_userinput(user_question)
 
